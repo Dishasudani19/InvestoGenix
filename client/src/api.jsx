cede00ac742f2c899d89ef76649d5c6a3f119b70
Disha Sudani <dishasudani01@gmail.com>
2025-03-01 11:16:39 +0530

    Basic Authentification

---
+++ client/src/api.jsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const API_URL = "http://127.0.0.1:8000/api/auth/";
+
+export const signup = async (userData) => {
+    console.log(userData);
+    
+    return await axios.post(`${API_URL}signup/`, userData);
+};
+
+export const login = async (userData) => {
+    console.log(userData);
+    return await axios.post(`${API_URL}login/`, userData);
+};
